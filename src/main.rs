52bf213b0a46a93e77080059c65711cb23de0900
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2025-11-04 22:51:39 -0800

    FEAT: added a currency system to my game

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use macroquad::prelude::{camera::mouse, *};
+use macroquad::rand::gen_range;
+
+#[macroquad::main("Taco Stand")]
+async fn main() {
+    let mut money = 0.0;
+    let mut coin_pos = vec2(100.0, 100.0);
+    let coin_radius = 20.0;
+    let mut coin_visible = true;
+    let mut last_spawn_time = get_time();
+    let mut respawn_delay = 1.0;
+
+    loop {
+        money = money + 0.01;
+        clear_background(RED);
+
+        if !coin_visible && get_time() - last_spawn_time > respawn_delay {
+            coin_pos = vec2(
+                gen_range(50.0, screen_width() - 50.0),
+                gen_range(100.0, screen_width() - 50.0),
+            );
+            coin_visible = true;
+        }
+        if coin_visible {
+            draw_circle(coin_pos.x, coin_pos.y, coin_radius, BLACK);
+            if is_mouse_button_pressed(MouseButton::Left) {
+                let (mx, my) = mouse_position();
+                let dx = mx - coin_pos.x;
+                let dy = my - coin_pos.y;
+                if dx * dx + dy * dy <= coin_radius * coin_radius {
+                    money += 1.0;
+                    coin_visible = false;
+                    last_spawn_time = get_time();
+                }
+            }
+        }
+
+        draw_text(
+            &format!("Currency: ${:.0}", money),
+            20.0,
+            40.0,
+            30.0,
+            DARKBROWN,
+        );
+        next_frame().await
+    }
+}
