b64366b8af92d3a6a429a3561bd55c36b52e668a
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2025-11-06 23:31:09 -0800

    FEAT: added a upgrade to increase the rate of tips

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use macroquad::prelude::{camera::mouse, *};
 use macroquad::rand::gen_range;
 
+fn repair_shop(respawn_delay: &mut f64) {
+    draw_rectangle(0.0, 0.0, 150.0, 100.0, WHITE);
+    draw_text("Repair Shop: Tip rate increase+", 10.0, 10.0, 10.0, BLACK);
+    if is_mouse_button_pressed(MouseButton::Left) {
+        let (mx, my) = mouse_position();
+        let rect_x = 0.0;
+        let rect_y = 0.0;
+        let rect_w = 100.0;
+        let rect_h = 100.0;
+
+        // Check if click is inside rectangle
+        if mx >= rect_x && mx <= rect_x + rect_w && my >= rect_y && my <= rect_y + rect_h {
+            *respawn_delay = 0.5;
+            println!("Repair Shop clicked! Respawn delay changed to 0.5s")
+        }
+    }
+}
 #[macroquad::main("Taco Stand")]
 async fn main() {
     let mut money = 0.0;
@@ -35,13 +52,8 @@ async fn main() {
             }
         }
 
-        draw_text(
-            &format!("Currency: ${:.0}", money),
-            20.0,
-            40.0,
-            30.0,
-            DARKBROWN,
-        );
+        repair_shop(&mut respawn_delay);
+        draw_text(&format!("Currency: ${:.0}", money), 20.0, 40.0, 30.0, BLACK);
         next_frame().await
     }
 }
